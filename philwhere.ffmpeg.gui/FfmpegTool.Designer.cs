4cd86da65334e519563de25383ef13e947295282
Philip Wong <philip.wong@genesisenergy.co.nz>
2022-01-03 03:44:18 +1300

    Add project files.

---
+++ philwhere.ffmpeg.gui/FfmpegTool.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace philwhere.ffmpeg.gui
+{
+    partial class FfmpegTool
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.cliTextBox = new System.Windows.Forms.TextBox();
+            this.aspectRatioGroupBox = new System.Windows.Forms.GroupBox();
+            this.ignoreAspectRatioCheckBox = new System.Windows.Forms.CheckBox();
+            this.aspectRatioTextBox = new System.Windows.Forms.TextBox();
+            this.audioGroupBox = new System.Windows.Forms.GroupBox();
+            this.ignoreDownmixCheckBox = new System.Windows.Forms.CheckBox();
+            this.prettyPrintCheckBox = new System.Windows.Forms.CheckBox();
+            this.cliPreviewGroupBox = new System.Windows.Forms.GroupBox();
+            this.aspectRatioGroupBox.SuspendLayout();
+            this.audioGroupBox.SuspendLayout();
+            this.cliPreviewGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Enabled = false;
+            this.runButton.Location = new System.Drawing.Point(487, 156);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 4;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // cliTextBox
+            // 
+            this.cliTextBox.Location = new System.Drawing.Point(6, 22);
+            this.cliTextBox.Multiline = true;
+            this.cliTextBox.Name = "cliTextBox";
+            this.cliTextBox.ReadOnly = true;
+            this.cliTextBox.Size = new System.Drawing.Size(556, 128);
+            this.cliTextBox.TabIndex = 3;
+            this.cliTextBox.TabStop = false;
+            this.cliTextBox.Text = "Drag file into form to get started";
+            // 
+            // aspectRatioGroupBox
+            // 
+            this.aspectRatioGroupBox.Controls.Add(this.ignoreAspectRatioCheckBox);
+            this.aspectRatioGroupBox.Controls.Add(this.aspectRatioTextBox);
+            this.aspectRatioGroupBox.Enabled = false;
+            this.aspectRatioGroupBox.Location = new System.Drawing.Point(46, 29);
+            this.aspectRatioGroupBox.Name = "aspectRatioGroupBox";
+            this.aspectRatioGroupBox.Size = new System.Drawing.Size(200, 61);
+            this.aspectRatioGroupBox.TabIndex = 4;
+            this.aspectRatioGroupBox.TabStop = false;
+            this.aspectRatioGroupBox.Text = "Aspect Ratio";
+            // 
+            // ignoreAspectRatioCheckBox
+            // 
+            this.ignoreAspectRatioCheckBox.AutoSize = true;
+            this.ignoreAspectRatioCheckBox.Location = new System.Drawing.Point(100, 25);
+            this.ignoreAspectRatioCheckBox.Name = "ignoreAspectRatioCheckBox";
+            this.ignoreAspectRatioCheckBox.Size = new System.Drawing.Size(60, 19);
+            this.ignoreAspectRatioCheckBox.TabIndex = 1;
+            this.ignoreAspectRatioCheckBox.Text = "Ignore";
+            this.ignoreAspectRatioCheckBox.UseVisualStyleBackColor = true;
+            this.ignoreAspectRatioCheckBox.CheckedChanged += new System.EventHandler(this.anything_Changed);
+            // 
+            // aspectRatioTextBox
+            // 
+            this.aspectRatioTextBox.Enabled = false;
+            this.aspectRatioTextBox.Location = new System.Drawing.Point(7, 23);
+            this.aspectRatioTextBox.Name = "aspectRatioTextBox";
+            this.aspectRatioTextBox.Size = new System.Drawing.Size(68, 23);
+            this.aspectRatioTextBox.TabIndex = 0;
+            this.aspectRatioTextBox.Text = "16:9";
+            this.aspectRatioTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.aspectRatioTextBox.TextChanged += new System.EventHandler(this.anything_Changed);
+            // 
+            // audioGroupBox
+            // 
+            this.audioGroupBox.Controls.Add(this.ignoreDownmixCheckBox);
+            this.audioGroupBox.Enabled = false;
+            this.audioGroupBox.Location = new System.Drawing.Point(252, 29);
+            this.audioGroupBox.Name = "audioGroupBox";
+            this.audioGroupBox.Size = new System.Drawing.Size(200, 61);
+            this.audioGroupBox.TabIndex = 5;
+            this.audioGroupBox.TabStop = false;
+            this.audioGroupBox.Text = "Merge Audio Channels";
+            // 
+            // ignoreDownmixCheckBox
+            // 
+            this.ignoreDownmixCheckBox.AutoSize = true;
+            this.ignoreDownmixCheckBox.Checked = true;
+            this.ignoreDownmixCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ignoreDownmixCheckBox.Location = new System.Drawing.Point(15, 25);
+            this.ignoreDownmixCheckBox.Name = "ignoreDownmixCheckBox";
+            this.ignoreDownmixCheckBox.Size = new System.Drawing.Size(60, 19);
+            this.ignoreDownmixCheckBox.TabIndex = 2;
+            this.ignoreDownmixCheckBox.Text = "Ignore";
+            this.ignoreDownmixCheckBox.UseVisualStyleBackColor = true;
+            this.ignoreDownmixCheckBox.CheckedChanged += new System.EventHandler(this.anything_Changed);
+            // 
+            // prettyPrintCheckBox
+            // 
+            this.prettyPrintCheckBox.AutoSize = true;
+            this.prettyPrintCheckBox.Checked = true;
+            this.prettyPrintCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.prettyPrintCheckBox.Location = new System.Drawing.Point(6, 156);
+            this.prettyPrintCheckBox.Name = "prettyPrintCheckBox";
+            this.prettyPrintCheckBox.Size = new System.Drawing.Size(82, 19);
+            this.prettyPrintCheckBox.TabIndex = 6;
+            this.prettyPrintCheckBox.Text = "PrettyPrint";
+            this.prettyPrintCheckBox.UseVisualStyleBackColor = true;
+            this.prettyPrintCheckBox.CheckedChanged += new System.EventHandler(this.anything_Changed);
+            // 
+            // cliPreviewGroupBox
+            // 
+            this.cliPreviewGroupBox.Controls.Add(this.prettyPrintCheckBox);
+            this.cliPreviewGroupBox.Controls.Add(this.cliTextBox);
+            this.cliPreviewGroupBox.Controls.Add(this.runButton);
+            this.cliPreviewGroupBox.Enabled = false;
+            this.cliPreviewGroupBox.Location = new System.Drawing.Point(46, 96);
+            this.cliPreviewGroupBox.Name = "cliPreviewGroupBox";
+            this.cliPreviewGroupBox.Size = new System.Drawing.Size(568, 195);
+            this.cliPreviewGroupBox.TabIndex = 7;
+            this.cliPreviewGroupBox.TabStop = false;
+            this.cliPreviewGroupBox.Text = "FFMPEG Arguments";
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 313);
+            this.Controls.Add(this.cliPreviewGroupBox);
+            this.Controls.Add(this.audioGroupBox);
+            this.Controls.Add(this.aspectRatioGroupBox);
+            this.Name = "FfmpegTool";
+            this.Text = "FFMPEG GUI";
+            this.aspectRatioGroupBox.ResumeLayout(false);
+            this.aspectRatioGroupBox.PerformLayout();
+            this.audioGroupBox.ResumeLayout(false);
+            this.audioGroupBox.PerformLayout();
+            this.cliPreviewGroupBox.ResumeLayout(false);
+            this.cliPreviewGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox cliTextBox;
+        private System.Windows.Forms.GroupBox aspectRatioGroupBox;
+        private System.Windows.Forms.CheckBox ignoreAspectRatioCheckBox;
+        private System.Windows.Forms.TextBox aspectRatioTextBox;
+        private System.Windows.Forms.GroupBox audioGroupBox;
+        private System.Windows.Forms.CheckBox ignoreDownmixCheckBox;
+        private System.Windows.Forms.CheckBox prettyPrintCheckBox;
+        private System.Windows.Forms.GroupBox cliPreviewGroupBox;
+    }
+}
+
---
+++ philwhere.ffmpeg.gui/FfmpegTool.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace philwhere.ffmpeg.gui
+{
+    public partial class FfmpegTool : Form
+    {
+        private FileInfo _file;
+        private string PrettyPrintFfmpegArguments =>
+            $"-i \"{_file.Name}\" " + Environment.NewLine +
+            AspectRatioArgument +
+            StereoDownmixArgument +
+            $"-c copy \"{Path.GetFileNameWithoutExtension(_file.FullName)}_done.mp4\"";
+        private string ActualFfmpegArguments =>
+            PrettyPrintFfmpegArguments.Replace(Environment.NewLine, string.Empty);
+        private string AspectRatioArgument => 
+            ignoreAspectRatioCheckBox.Checked ? null : $"-aspect {aspectRatioTextBox.Text} " + Environment.NewLine;
+        private string StereoDownmixArgument =>
+            ignoreDownmixCheckBox.Checked ? null : "-ac 1 " + Environment.NewLine;
+
+        public FfmpegTool()
+        {
+            InitializeComponent();
+            AllowDrop = true;
+            DragEnter += Form_DragEnter;
+            DragDrop += Form_DragDrop;
+        }
+
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "ffmpeg.exe",
+                    WorkingDirectory = _file.DirectoryName,
+                    Arguments = ActualFfmpegArguments,
+                    UseShellExecute = true
+                }
+            };
+            process.Start();
+            process.EnableRaisingEvents = true;
+            process.Exited += (_, _) => MessageBox.Show("Done");
+        }
+
+        private void Form_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form_DragDrop(object sender, DragEventArgs e) {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 1)
+            {
+                MessageBox.Show("1 file at a time.");
+                return;
+            }
+
+            _file = new FileInfo(files.Single());
+            TabIndex = 0;
+            UpdateEverything();
+        }
+
+        private void anything_Changed(object sender, EventArgs e)
+        {
+            UpdateEverything();
+        }
+
+
+        private void UpdateEverything()
+        {
+            if (_file.Exists)
+            {
+                runButton.Enabled = true;
+                aspectRatioGroupBox.Enabled = true;
+                audioGroupBox.Enabled = true;
+                cliPreviewGroupBox.Enabled = true;
+                aspectRatioTextBox.Enabled = !ignoreAspectRatioCheckBox.Checked;
+                aspectRatioTextBox.Font = new Font(aspectRatioTextBox.Font,
+                    ignoreAspectRatioCheckBox.Checked ? FontStyle.Strikeout : FontStyle.Regular);
+            }
+            cliTextBox.Text = prettyPrintCheckBox.Checked ? PrettyPrintFfmpegArguments : ActualFfmpegArguments;
+        }
+    }
+}
